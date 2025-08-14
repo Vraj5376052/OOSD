a4393969be33eadec5916cf1e401623d8796850d
tom-davis2 <tom.davis2@griffithuni.edu.au>
2025-08-14 10:08:40 +1000

    Tom attempted to merge HIghScoreScreen within main, however errors are showing.

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,13 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -69,12 +72,14 @@ public class Main extends Application {
         // Main menu buttons
         Button btnConfigure = new Button("Configure");
         Button btnPlay = new Button("Play");
+        Button btnHighScores = new Button("High Scores");
         Button exitButton = new Button("Exit");
         exitButton.setOnAction(e -> showExitConfirmation(stage));
 
 
         btnConfigure.setOnAction(e -> showConfigScreen(primaryStage));
         btnPlay.setOnAction(e -> showPlayScreen(primaryStage));
+        btnHighScores.setOnAction(e -> showHighScoreScreen(primaryStage));
         btnExit.setOnAction(e -> primaryStage.close());
 
         VBox mainLayout = new VBox(20, btnConfigure, btnPlay, btnExit);
@@ -472,4 +477,75 @@ public class Exit extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-}
+    private void showScoresScreen(Stage stage) {
+
+            primaryStage.setTitle("Tetris");
+
+            // Root layout
+            VBox root = new VBox(15);
+            root.setAlignment(Pos.TOP_CENTER);
+
+            // Title
+            Label title = new Label("High Score");
+            title.setFont(Font.font("SansSerif", FontWeight.BOLD, 24));
+            root.getChildren().add(title);
+
+            // GridPane for the table
+            GridPane grid = new GridPane();
+            grid.setHgap(40);
+            grid.setVgap(10);
+            grid.setAlignment(Pos.CENTER);
+
+            // Column headers
+            Label nameHeader = new Label("Name");
+            nameHeader.setFont(Font.font("SansSerif", FontWeight.BOLD, 16));
+            Label scoreHeader = new Label("Score");
+            scoreHeader.setFont(Font.font("SansSerif", FontWeight.BOLD, 16));
+
+            grid.add(nameHeader, 0, 0);
+            grid.add(scoreHeader, 1, 0);
+
+            // Sample data
+            String[][] data = {
+                    {"Vraj", "869613"},
+                    {"Tom", "754569"},
+                    {"Anh", "642871"},
+                    {"Jack", "549280"},
+                    {"Luis", "537728"},
+                    {"Tom", "462740"},
+                    {"Vraj", "366765"},
+                    {"Vraj", "326181"},
+                    {"Luis", "301649"},
+                    {"Jack", "260598"},
+            };
+            // Populate GridPane with data
+            for (int i = 0; i < data.length; i++) {
+                Label name = new Label(data[i][0]);
+                name.setFont(Font.font("SansSerif", 14));
+                Label score = new Label(data[i][1]);
+                score.setFont(Font.font("SansSerif", 14));
+                grid.add(name, 0, i + 1);
+                grid.add(score, 1, i + 1);
+            }
+
+            root.getChildren().add(grid);
+
+            // Back button
+            Button backButton = new Button("Return to Menu");
+            backButton.setPrefWidth(100);
+            root.getChildren().add(backButton);
+
+
+            // Scene and stage
+           Scene scene = new Scene(root, 450, 500);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+
+        //public static void main(String[] args) {
+         //   launch(args);
+  //      }
+
+
+    }
+
