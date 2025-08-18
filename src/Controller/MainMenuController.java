3fd36c5a7b43b215d02c4ccf8f2b59176876d203
AnhTran-ICT <voducanh.tran@griffithuni.edu.au>
2025-08-18 14:13:44 +1000

    Anh update the code, adding pop-up for quitting the game/app, pausing the game

---
+++ src/Controller/MainMenuController.java
@@ -0,0 +1,44 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.application.Platform;
+
+public class MainMenuController {
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private Button configButton;
+
+    @FXML
+    private Button scoreButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private void handlePlay() {
+        System.out.println("Play clicked - Launch game screen");
+        // TODO: Load gameplay scene
+    }
+
+    @FXML
+    private void handleConfig() {
+        System.out.println("Configuration clicked - Open configuration screen");
+        // TODO: Load configuration screen
+    }
+
+    @FXML
+    private void handleHighScores() {
+        System.out.println("High Scores clicked - Show scores");
+        // TODO: Load high score screen
+    }
+
+    @FXML
+    private void handleExit() {
+        Platform.exit();
+    }
+}
